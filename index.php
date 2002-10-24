7461f7a352f07cd0c9ee230edd6741eee65b85ff
rvgnu <rvgnu>
2002-10-24 16:25:48 +0000

    Update copyright on all files and add Makefile to ease the release process.

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//    If you have any problems to report, or suggestions, please send them
+//    to p4el-bugs@lists.sourceforge.net
 
 function fstats($file, &$kbsize)
 {
