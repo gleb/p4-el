c0d6aeb1b3f3a714d6888f615dd4c1ba9cc74ab5
petero2 <petero2>
2005-03-27 16:41:29 +0000

    Also ask for p4 password if the session has expired.

--- p4.el
+++ p4.el
@@ -478,7 +478,8 @@ Does auto re-highlight management (whatever that is)."
 		    args)))
 	(goto-char (point-min))
 	(if (and (not no-login)
-		 (looking-at "Perforce password (P4PASSWD) invalid or unset"))
+		 (or (looking-at "Perforce password (P4PASSWD) invalid or unset")
+		     (looking-at "Your session has expired, please login again.")))
 	    (progn
 	      (setq current-prefix-arg nil)
 	      (p4-login)
